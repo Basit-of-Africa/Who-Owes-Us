aee51118257da8e6e8c44fcfe57e530cca2da8b0
AbdulBasit Ajibade <ajibadebasit40@gmail.com>
2026-03-26 16:19:57 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/lib/politician-names.ts
+++ src/lib/politician-names.ts
@@ -2,30 +2,45 @@
 /**
  * A comprehensive list of prominent Nigerian political figures 
  * across Federal, State, and Legislative branches.
- * Reference: https://p.placbillstrack.org/members/
+ * This list serves as a seed for the AI Discovery engine.
  */
 export const PROMINENT_NIGERIAN_POLITICIANS = [
-  // Federal Executive
+  // Federal Executive (Current & Recent)
   "Bola Ahmed Tinubu", "Kashim Shettima", "Nyesom Wike", "Godswill Akpabio", "Godwin Emefiele",
   "Olayemi Cardoso", "Yusuf Tuggar", "Lateef Fagbemi", "Muhammad Ali Pate", "Olawale Edun",
+  "Festus Keyamo", "Dave Umahi", "Betta Edu", "Bello Matawalle", "Badaru Abubakar",
+  "George Akume", "Femi Gbajabiamila", "Ajuri Ngelale", "Heidi Sirika", "Zainab Ahmed",
   
-  // Senate & House (PLAC Reference)
-  "Barau Jibrin", "Opeyemi Bamidele", "Ali Ndume", "Abba Moro", "Dave Umahi",
-  "Saliu Mustapha", "Adams Oshiomhole", "Seriake Dickson", "Gbenga Daniel", "Enyinnaya Abaribe",
+  // 10th Senate (Subset of 109)
+  "Barau Jibrin", "Opeyemi Bamidele", "Ali Ndume", "Abba Moro", "Saliu Mustapha", 
+  "Adams Oshiomhole", "Seriake Dickson", "Gbenga Daniel", "Enyinnaya Abaribe", "Ned Nwoko",
+  "Ifeanyi Ubah", "Jimoh Ibrahim", "Orji Uzor Kalu", "Danjuma Goje", "Ahmad Lawan",
+  "Aminu Tambuwal", "Ibrahim Dankwambo", "Gbenga Daniel", "Shuaibu Lau", "Lola Ashiru",
+  "Kalu Okechukwu", "Tony Nwoye", "Victor Umeh", "Sani Musa", "Jibrin Isah",
+  "Binos Yaroe", "Ishaku Abbo", "Elisha Abbo", "Sahabi Ya'u", "Yahaya Abdullahi",
+  
+  // 10th House of Reps (Subset of 360)
   "Tajudeen Abbas", "Benjamin Kalu", "Kingsley Chinda", "Ikechi Emenike", "Leke Abejide",
+  "Bamidele Salam", "Afam Ogene", "James Faleke", "Muktar Betara", "Idris Wase",
+  "Alhassan Ado-Doguwa", "Sada Soli", "Jimi Benson", "Femi Bamisile", "Beni Lar",
   
-  // Governors
+  // Current Governors (36 States)
   "Babajide Sanwo-Olu", "Seyi Makinde", "Abba Kabir Yusuf", "Uba Sani", "Siminalayi Fubara",
   "Alex Otti", "Peter Mbah", "Bassey Otu", "Sheriff Oborevwori", "Francis Nwifuru",
   "Agbu Kefas", "Daudu Lawal", "Caleb Mutfwang", "Mohammed Bago", "Umar Namadi",
   "Dikko Radda", "Nasir Idris", "Inuwa Yahaya", "Bala Mohammed", "Babagana Zulum",
   "Mai Mala Buni", "Ahmadu Fintiri", "Abdullahi Sule", "Yahaya Bello", "Lucky Aiyedatiwa",
-  "Abiodun Oyebanji", "Dapo Abiodun", "Ademola Adeleke", "Gboyega Oyetola", "Kayode Fayemi",
+  "Abiodun Oyebanji", "Dapo Abiodun", "Ademola Adeleke", "Gboyega Oyetola", "Godwin Obaseki",
+  "Douye Diri", "Charles Soludo", "Hope Uzodinma", "Ahmed Ododo", "Hyacinth Alia", "Umo Eno",
   
   // Historical/Prominent Figures (Last 12 Years)
   "Atiku Abubakar", "Peter Obi", "Bukola Saraki", "Ike Ekweremadu", "Diezani Alison-Madueke",
-  "James Ibori", "Orji Uzor Kalu", "Rochas Okorocha", "Dino Melaye", "Hadi Sirika",
-  "Sambo Dasuki", "Stella Oduah", "Femi Fani-Kayode", "Ayo Fayose", "Rauf Aregbesola",
-  "Nasir El-Rufai", "Abdullahi Ganduje", "Aminu Tambuwal", "Rabiu Kwankwaso", "Goodluck Jonathan",
-  "Namadi Sambo", "Patience Jonathan", "Mohammed Bello Adoke", "Bala Mohammed", "Tukur Buratai"
+  "James Ibori", "Rochas Okorocha", "Dino Melaye", "Sambo Dasuki", "Stella Oduah", 
+  "Femi Fani-Kayode", "Ayo Fayose", "Rauf Aregbesola", "Nasir El-Rufai", "Abdullahi Ganduje",
+  "Rabiu Kwankwaso", "Goodluck Jonathan", "Namadi Sambo", "Patience Jonathan", "Bala Mohammed",
+  "Mohammed Bello Adoke", "Tukur Buratai", "Gabriel Suswam", "Babangida Aliyu", "Isa Yuguda",
+  "Sullivan Chime", "Martin Elechi", "Theodore Orji", "Liyel Imoke", "Godswill Akpabio",
+  "Rotimi Amaechi", "Murtala Nyako", "Tanko Al-Makura", "Kashim Shettima", "Ibrahim Dankwambo",
+  "Rochas Okorocha", "Abiola Ajimobi", "Ibikunle Amosun", "Adams Oshiomhole", "Abdul'aziz Yari",
+  "Kayode Fayemi", "Olusegun Mimiko", "Seriake Dickson", "Ibrahim Geidam", "Idris Wada"
 ];
